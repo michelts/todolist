ed9fd3800c1e4cbc125afa574d9fa03e10540d5a
Michel Sabchuk <michel@turbosys.com.br>
2020-05-21 20:07:42 -0300

    Implementation of the creation view.

--- app/views.py
+++ app/views.py
@@ -1,17 +1,30 @@
-from flask import jsonify
+import json
+from flask import request, jsonify, abort
 from flask.blueprints import Blueprint
-from flask_login import login_required
-from .models import Task
+from flask_login import login_required, current_user
+
+from . import models, validators
 
 blueprint = Blueprint("todos", __name__)
 
 
-@blueprint.route("/tasks/")
+@blueprint.route("/tasks/", methods=["GET"])
 @login_required
 def task_list():
-    return jsonify(
-        [
-            obj.serialize()
-            for obj in Task.query.all()
-        ]
-    )
+    return jsonify([obj.serialize() for obj in models.Task.query.all()])
+
+
+@blueprint.route("/tasks/", methods=["POST"])
+@login_required
+def task_create():
+    schema = validators.TaskSchema()
+    errors = schema.validate(request.json)
+    if errors:
+        return abort(400, json.dumps(errors))
+
+    payload = schema.load(request.json)
+    obj = models.Task(user_id=current_user.id, **payload)
+    models.db.session.add(obj)
+    models.db.session.commit()
+
+    return jsonify(obj.serialize()), 201
