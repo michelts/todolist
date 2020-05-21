81b5a38d84be6fbf626e3fd6d42ab0522faf4908
Michel Sabchuk <michel@turbosys.com.br>
2020-05-21 20:05:30 -0300

    Simplify representation of Task (that is used for debug).

--- app/models.py
+++ app/models.py
@@ -39,20 +39,14 @@ def load_user(user_id):
 class Task(db.Model):
     __tablename__ = "task"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     priority = db.Column(db.Integer, default=0)
     due_date = db.Column(db.Date, nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return "<Todo id={id} task={task} priority={priority} due_date={due_date} completed={completed}>".format(
-            id=self.id,
-            task=self.task[:30],
-            priority=self.priority,
-            due_date=self.due_date.strftime("%Y-%m-%d"),
-            completed="yes" if self.completed else "no",
-        )
+        return "<Todo id={id}>".format(id=self.id)
 
     def serialize(self):
         return {
