a2fd359717fea2d9b69e8a94a3f871af460a5174
Michel Sabchuk <michel@turbosys.com.br>
2020-05-22 12:41:13 -0300

    Auto mock react-router-again and disable it only when needed.

--- assets/app/components/__tests__/Index.test.js
+++ assets/app/components/__tests__/Index.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Index from '../Index';
 
+jest.unmock('react-router-dom');
+
 describe('Index component', () => {
   it('should render routes to Login, Logout and Tasks', () => {
     const wrapper = shallow(<Index />);
