62069f9e4a285ac8b033b8b697ab2a1feeeb6d22
Michel Sabchuk <michel@turbosys.com.br>
2020-05-22 08:31:34 -0300

    Test creation with all fields.

--- tests/test_task_create.py
+++ tests/test_task_create.py
@@ -1,5 +1,5 @@
 import pytest
-from datetime import datetime
+from datetime import date
 
 from app import models
 
@@ -36,12 +36,27 @@ def test_task_create_saves_and_returns_it_for_authenticated_user(
     }
 
 
-def test_task_creation_fails_when_description_is_missing(
+def test_task_create_saves_priority_due_date_and_is_completed_flag_when_available(
     user_client, user, payload
 ):
-    del payload['description']
+    payload["priority"] = 4
+    payload["due_date"] = date(2020, 5, 22).isoformat()
+    payload["is_completed"] = True
     response = user_client.post("/api/v1/tasks/", json=payload)
-    assert response.status_code == 400
+    assert response.status_code == 201
+
+    obj = models.Task.query.one()
+    assert obj.priority == payload["priority"]
+    assert obj.due_date.isoformat() == payload["due_date"]
+    assert obj.is_completed == payload["is_completed"]
+
+    assert response.json == {
+        "id": obj.id,
+        "description": obj.description,
+        "priority": obj.priority,
+        "due_date": obj.due_date.isoformat(),
+        "is_completed": obj.is_completed,
+    }
 
 
 def test_task_update_saves_null_due_date(user_client, user, payload):
@@ -53,10 +68,16 @@ def test_task_update_saves_null_due_date(user_client, user, payload):
     assert obj.due_date is None
 
 
-@pytest.mark.parametrize('invalid_value', ['some-invalid-value', ''])
+def test_task_creation_fails_when_description_is_missing(user_client, user, payload):
+    del payload["description"]
+    response = user_client.post("/api/v1/tasks/", json=payload)
+    assert response.status_code == 400
+
+
+@pytest.mark.parametrize("invalid_value", ["some-invalid-value", ""])
 def test_task_creation_fails_when_due_date_is_invalid(
     user_client, user, payload, invalid_value
 ):
-    payload['due_date'] = invalid_value
+    payload["due_date"] = invalid_value
     response = user_client.post("/api/v1/tasks/", json=payload)
     assert response.status_code == 400
